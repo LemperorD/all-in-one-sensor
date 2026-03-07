ce66b40278c0c57570f84e33038c08b83649b74b
LD <ld2382619813@163.com>
2026-03-08 00:49:36 +0800

    refactor(yolo): :recycle: 更改yolo部分模块为开源项目yolo_ros
    fork他人的开源项目并做出一定修改，增加权重存放与参数存放的文件夹

--- src/yolo_detector/datasets/README.md
+++
@@ -1,3 +0,0 @@
-# don't delete it & f**k git!
-
-[数据集下载链接]()
--- src/yolo_detector/yolo_detector/yolo_detector.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo26n.pt")  # load an official model
-model = YOLO("path/to/best.pt")  # load a custom-trained model
-
-# Export the model
-model.export(format="onnx")
