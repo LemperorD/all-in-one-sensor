96afa53589d1694854c4b72ccf48bdead0d82c3e
LemperorD <ld2382619813@163.com>
2026-05-07 20:23:22 +0800

    feat(build): 更新colcon构建命令，添加ROS版本和发行版参数

--- scripts/colconBuild.sh
+++ scripts/colconBuild.sh
@@ -1,2 +1,3 @@
 cd ~/all-in-one-sensor
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DROS_EDITION="ROS2" -DDISTRO_ROS="humble"
